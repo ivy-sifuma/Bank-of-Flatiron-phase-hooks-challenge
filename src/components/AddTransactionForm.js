336743c045938347f568a0fdae0d7e631c5f6de5
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-09-25 19:03:27 +0300

    Bank

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -25,10 +25,13 @@ function handleSubmitAction(event) {
     <div className="ui segment">
       <form onSubmit={handleSubmitAction} onChange={handleChangeAction} className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input value={formData.date} type="date" name="date" />
+          <input 
+          value={formData.description} type="text" name="description" placeholder="Description" />
+          <input 
+          value={formData.category} type="text" name="category" placeholder="Category" />
+          <input 
+          value={formData.amount} type="number" name="amount" placeholder="Amount" step="0.01" />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
