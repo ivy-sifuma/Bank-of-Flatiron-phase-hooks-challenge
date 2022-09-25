dd312978008d88e0ac0cdf1e59936d1f00dafbd3
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-09-25 21:51:51 +0300

    Bank

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+
+  const [transactions, setTransactions] = useState([]);
+  //const [searchTransaction, setSearchTransaction] = useState("")
+  useEffect( () => {
+    fetch("http://localhost:8001/transactions")
+    .then( (res) => res.json())
+    .then((trans) => setTransactions (trans));
+  }, []);
+
+  console.log(transactions);
+
+  function handleUpdateOnSubmitting(newTransaction) {
+    //console.log(newTransaction);
+    setTransactions(transactions => [ ...transactions, newTransaction]);
+  }
   return (
     <div>
       <Search />
